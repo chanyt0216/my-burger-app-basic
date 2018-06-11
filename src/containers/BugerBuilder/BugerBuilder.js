3c3d0417ffe4f6ba9c36d7fe50846a271602f9d7
yt_chan <yt_chan@kinetix.com.hk>
2018-06-11 18:47:27 +0800

    creating the order summary modal

--- src/containers/BugerBuilder/BugerBuilder.js
+++ src/containers/BugerBuilder/BugerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -26,7 +28,6 @@ class BurgerBuilder extends Component {
     }
 
     updatePurchaseState = (ingredients) => {
-
         const sum = Object.keys(ingredients)
             .map(igKey => {
                 return ingredients[igKey];
@@ -91,6 +92,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
