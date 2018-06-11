e7e9e3432325066da96ea24fa2d07c54330e28fe
yt_chan <yt_chan@kinetix.com.hk>
2018-06-11 15:35:21 +0800

    adding the order button

--- src/containers/BugerBuilder/BugerBuilder.js
+++ src/containers/BugerBuilder/BugerBuilder.js
@@ -21,7 +21,21 @@ class BurgerBuilder extends Component {
                 meat: 0
             },
             totalPrice: 4,
+            purchaseable: false,
         }
+    }
+
+    updatePurchaseState = (ingredients) => {
+
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el
+            }, 0);
+
+        this.setState({ purchaseable: sum > 0 });
     }
 
     addIngredientHandler = (type) => {
@@ -39,6 +53,8 @@ class BurgerBuilder extends Component {
             totalPrice: newPrice,
             ingredients: updatedIngredients
         })
+
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -60,6 +76,8 @@ class BurgerBuilder extends Component {
             totalPrice: newPrice,
             ingredients: updatedIngredients
         })
+
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
@@ -78,6 +96,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
                     price={this.state.totalPrice}
                 />
             </Aux>
