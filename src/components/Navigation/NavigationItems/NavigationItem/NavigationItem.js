a87485ce393928b6e3e5982e35081979023508bc
yt_chan <yt_chan@kinetix.com.hk>
2018-10-15 15:18:49 +0800

    implementing navigation links

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import classes from './NavigationItem.less';
+import { NavLink } from "react-router-dom";
+import React from "react";
+import classes from "./NavigationItem.less";
 
-const navigationItem = (props) => {
-    return (
-        <li className={classes.NavigationItem}>
-            <a
-                href={props.link}
-                className={props.active ? classes.active : null}
-            >
-                {props.children}
-            </a>
-        </li>
-    );
+const navigationItem = props => {
+  return (
+    <li className={classes.NavigationItem}>
+      <NavLink
+        to={props.link}
+        activeClassName={classes.active}
+        exact={props.exact}
+      >
+        {props.children}
+      </NavLink>
+    </li>
+  );
 };
 
 export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import classes from './NavigationItems.less';
-import NavigationItem from './NavigationItem/NavigationItem';
+import React from "react";
+import classes from "./NavigationItems.less";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = (props) => {
-    return (
-        <ul className={classes.NavigationItems}>
-            <NavigationItem link="/" active>Burger Builder</NavigationItem>
-            <NavigationItem link="/" >Checkout</NavigationItem>
-        </ul>
-    );
+const navigationItems = props => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/" exact>
+        Burger Builder
+      </NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    </ul>
+  );
 };
 
 export default navigationItems;
