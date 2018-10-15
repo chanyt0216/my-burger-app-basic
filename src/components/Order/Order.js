f8aa50e812d762db034c4bf9e3fbe80d0d3e1738
yt_chan <yt_chan@kinetix.com.hk>
2018-10-15 16:27:21 +0800

    219 Outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,43 @@ import React from "react";
 import classes from "./Order.less";
 
 const Order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutoput = ingredients.map(ig => {
+    return (
+      <span
+        key={ig.name}
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: </p>
+      <p>Ingredients: {ingredientOutoput}</p>
       <p>
-        Price: <strong>USD 5.45</strong>
+        Price: <strong>USD {props.price.toFixed(2)}</strong>
+        {/* 
+          (+props.price).toFixed(2)
+          or 
+          Number.parseFloat(props.price).toFixed(2)
+          Both can work 
+        */}
       </p>
     </div>
   );
