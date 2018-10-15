d2646a97ed903b11f6edf862e8df62bbb60e55ed
yt_chan <yt_chan@kinetix.com.hk>
2018-10-15 11:30:43 +0800

    navigatin to the contact data component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BugerBuilder from "./containers/BugerBuilder/BugerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" component={BugerBuilder} />
           </Switch>
         </Layout>
